c15fde6552859b78a2f72c0f2e256d71f29ad7b8
Ripat-2345 <ripatalsafar@gmail.com>
2022-02-09 22:00:47 +0700

    add theme's color and font

---
+++ lib/const.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+// * Theme color
+Color darkColor = const Color(0XFF21325E);
+Color blueColor = const Color(0XFF3E497A);
+Color yellowColor = const Color(0XFFF1D00A);
+Color whiteColor = const Color(0XFFF0F0F0);
--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,8 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      theme: ThemeData(fontFamily: 'Lemon'),
       debugShowCheckedModeBanner: false,
       home: GameScreen(),
     );
