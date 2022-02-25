6d2681fec52819d3d3e93e4322e02dcd6538fa82
Ripat-2345 <ripatalsafar@gmail.com>
2022-02-25 15:07:44 +0700

    setup firebase for flutter web

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,20 @@ import 'package:flutter/material.dart';
 import 'package:flutter_game/screens/feed_back_screen.dart';
 import 'package:flutter_game/screens/game_screen.dart';
 import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 import 'screens/home_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: const FirebaseOptions(
+      apiKey: "AIzaSyBcXakFFDhCHffDoP_OQWeqaZYyHhHIA14",
+      appId: "1:353073169291:web:244fd5c7ed41000a06658e",
+      messagingSenderId: "353073169291",
+      projectId: "traffic-light-puzzle",
+    ),
+  );
   runApp(const MyApp());
 }
 
